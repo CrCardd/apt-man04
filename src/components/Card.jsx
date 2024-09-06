6acae8afb57e814850085fa7885c0c6867c6cc80
CrCardd <cris.rc377@gmail.com>
2024-09-06 14:24:27 -0300

    flex-wrap, produtos adicionados e status visivel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
-/* eslint-disable react/prop-types */
-export const Card = (props) => {
+import style from '../components/card.module.css';
+
+
+export const Card = ({name,desc,value,image,status}) => {
   return(
-      <div>
-          <h1>{props.name}</h1>
-          <h2>{props.desc}</h2>
-          <p>{props.value}</p>
-          <img src={props.image} alt={props.name} width={150} height={"auto"}/>
+      <div className={style.card}>
+          <h1>{name}</h1>
+          <h2>{desc}</h2>
+          <p>{value}</p>
+          <div>
+            <img src={'../../src/assets/' + image} alt={name} width={150} height={"auto"}/>
+            <div className={status ? style.true : style.false}></div>
+          </div>
       </div>
   )
 }
